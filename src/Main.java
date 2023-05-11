a4fc4e71446ec0fe9021be7b126861fd9f18d656
Akansha Pal <apal@spartaglobal.com>
2023-05-11 17:07:33 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int count = 3;
+        String name = "Cathy";
+        boolean isHappy = true;
+        boolean done = false;
+        int newCount = count * 2;
+        newCount++;
+        if (isHappy) {
+            System.out.println("That's great");
+        } else {
+            System.out.println("I'm sorry");
+        }
+        if (count < 3) {
+            System.out.println("Lower");
+        } else if (count > 3) {
+            System.out.println("Higher");
+        } else {
+            System.out.println("Same");
+        }
+        for (int i = 0; i < newCount; i++) {
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+}
